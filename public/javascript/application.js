f11fd0bd373a4a24d6f30cda718df7a949b6a6ce
Gabriel Osborne <osbornegabriel@gmail.com>
2018-11-01 13:53:41 -0400

    Build foundation of equation form request, create names for form

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -9,10 +9,19 @@ calculate = function(){
   $('#equation-form').on("submit", function(event){
     event.preventDefault();
     var $form = $(this);
-    var formUrl = $form.attr('url');
+    var formUrl = $form.attr('action');
     var formMethod = $form.attr('method');
     var formData = $form.serialize();
-
+    console.log(formData);
+    console.log("bacon")
+    $.ajax({
+      url: formUrl,
+      method: formMethod,
+      data: formData
+    }).done(function(response){
+      console.log("in the response section");
+      console.log(response);
+    })
     console.log("The equation was submitted >)")
   })
 }
